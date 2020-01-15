c29734d63463e1b3201f648876954fc19ec30be4
Pavel Boltromyuk <beltring99@gmail.com>
2020-01-15 17:17:09 +0300

    Task complete.

--- Task1/StringHelper.cs
+++ Task1/StringHelper.cs
@@ -2,12 +2,38 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// The StringHelper class makes it easier to work with strings.
+    /// </summary>
     public static class StringHelper
     {
+        /// <summary>
+        /// Method for counting the number of vowel letters in a string.
+        /// </summary>
+        /// <param name="str">Input string.</param>
+        /// <returns>The method returns the number of vowel letters in a string.</returns>
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str), "String cannot be null.");
+            }
+
+            if (str.Length == 0)
+            {
+                throw new ArgumentException("String cannot be empty.");
+            }
+
+            int count = 0;
+            foreach (var ch in str)
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Task2
 {
@@ -6,8 +6,29 @@ namespace Task2
     {
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a == 0)
+            {
+                return Math.Abs(b);
+            }
+
+            if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+
+            while (b != 0)
+            {
+                int tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+
+            return Math.Abs(a);
         }
     }
 }
